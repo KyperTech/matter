87b75a0e455bdaa9f66d62f0ae8722c4aa80c1d0
Scott Prue <scott.prue@borderfree.com>
2016-01-22 15:26:28 -0500

    ES6 Promise added to dev deps so tests pass in travis.

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,7 @@
-// import { jsdom } from 'jsdom'
-// global.document = jsdom('<!doctype html><html><body></body></html>')
-// global.window = document.defaultView;
-// global.navigator = global.window.navigator;
-// global.window.sessionStorage = {};
 global.sinon = require('sinon');
 global.chai = require('chai');
 global.expect = require('chai').expect;
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
+var Promise = require('es6-promise').Promise;
+global.Promise = Promise;
